75aa92fa15affeaceef4a61e29996b881446ebaa
Juannn10 <Juanrezel@upi.edu>
2026-04-07 15:15:29 +0700

    Merubah Img

--- Perantau/HasilPage.js
+++ Perantau/HasilPage.js
@@ -7,20 +7,20 @@ var result = localStorage.getItem('interiology-result');
 if (result == 'scandinavian') {
     titleEl.textContent = "Si Hangat Scandinavian";
     descEl.textContent = "Kamu menyukai ruangan yang hangat dan nyaman dengan sentuhan natural...";
-    imgEl.src = "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?w=800";
+    imgEl.src = "Image/img-Scandinavian.png";
 } 
 else if (result == 'modern') {
     titleEl.textContent = "Si Elegan Modern";
     descEl.textContent = "Kamu suka ruangan yang terlihat mewah dan berkelas...";
-    imgEl.src = "https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?w=800";
+    imgEl.src = "Image/img-Modern.png";
 } 
 else if (result == 'bohemian') {
     titleEl.textContent = "Si Ceria Bohemian";
     descEl.textContent = "Kamu suka ruangan yang penuh warna dan karakter!...";
-    imgEl.src = "https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?w=800";
+    imgEl.src = "Image/img-Bohemian.png";
 } 
 else {
     titleEl.textContent = "Si Kalem Minimalis";
     descEl.textContent = "Kamu cocok terhadap furnitur yang berbahan kayu dan berbentuk simple...";
-    imgEl.src = "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=800";
+    imgEl.src = "Image/img-Minimalis.png";
 }
