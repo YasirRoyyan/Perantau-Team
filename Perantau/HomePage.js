cf748d060fd648047a696144562da079ffcd0dd2
Juannn10 <Juanrezel@upi.edu>
2026-04-07 12:25:24 +0700

    Merevisi Sedikit

--- Perantau/HomePage.js
+++ Perantau/HomePage.js
@@ -16,3 +16,13 @@ for (var i = 0; i < smoothLinks.length; i++) {
         });
     });
 }
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled'); 
+    } else {
+        navbar.classList.remove('scrolled'); 
+    }
+});
