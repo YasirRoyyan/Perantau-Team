ae56dc25e8ca14a1bc2c30cbb6386786203957af
minologic <ilhamporindo.ra@gmail.com>
2026-06-07 08:04:45 +0700

    menambahkan intended route (menyesuaikan alur kustom-ruangan setelah page result)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->route('home');
+        return redirect()->intended(route('home'));
     }
 
     public function showRegister(): View
